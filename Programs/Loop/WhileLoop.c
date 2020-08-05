a626c5df4c90a5f3a18bb3f29b8b955e0e2892de
Nabin <nabin.304@gmail.com>
2020-08-05 17:03:47 +0200

    While loop

---
+++ Programs/Loop/WhileLoop.c
@@ -0,0 +1,14 @@
+/*
+    Demo: While loop.
+    Info: Prints "Hello world" 10 times using while loop.
+*/
+#include <stdio.h>
+void main()
+{
+    int count=1;
+    while(count<=10){
+        printf("Hello world\n");
+        count++;
+    }
+}
+
